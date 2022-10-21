5955c914e4ae8880687ac4ed28b2ebfc31908cd9
Avishkar30 <90127433+Avishkar30@users.noreply.github.com>
2022-10-21 12:03:27 +0530

    Add files via upload

---
+++ Face-Emotion-Recognition/README.md
@@ -0,0 +1,15 @@
+# Face-Emotion-Recognition
+The Data set Link :- https://www.kaggle.com/jonathanoheix/face-expression-recognition-dataset
+And You can Download the trained model from following link :- https://drive.google.com/file/d/1YGW0WUhULQ_yhoVT_LtT9DH2WSG-Tn7z/view?usp=sharing
+
+
+Here Is the complete guide for Implementing this project 
+1. Download the code uploaded here or you can use the ipynb jupyter file for reference and build your own model
+2. If you are not building your own model, you can dowmload the pre trained model from the Google drive link present above
+3. Once you have the model, implement the main.py file in any python Integrated IDE such as Spyder, Pycharm etc.
+4. After the implementation when you run the file live camera will be opened and it will classify the face emotions
+
+
+
+
+Thank You!!
---
+++ Face-Emotion-Recognition/main.py
@@ -0,0 +1,46 @@
+from keras.models import load_model
+from time import sleep
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
+import cv2
+import numpy as np
+
+face_classifier = cv2.CascadeClassifier(r'C:\Users\Admin\Desktop\PythonProject\EmotionDetectionCNN\haarcascade_frontalface_default.xml')
+classifier =load_model(r'C:\Users\Admin\Desktop\PythonProject\EmotionDetectionCNN\model.h5')
+
+emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
+
+cap = cv2.VideoCapture(0)
+
+
+
+while True:
+    _, frame = cap.read()
+    labels = []
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+
+
+        if np.sum([roi_gray])!=0:
+            roi = roi_gray.astype('float')/255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi,axis=0)
+
+            prediction = classifier.predict(roi)[0]
+            label=emotion_labels[prediction.argmax()]
+            label_position = (x,y)
+            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+        else:
+            cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+    cv2.imshow('Emotion Detector',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
