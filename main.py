fd3779ae9a92424735259a49ae0a37300cc2d24f
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2023-03-18 08:07:26 +0530

    face detection was solved

---
+++ main.py
@@ -0,0 +1,37 @@
+import cv2
+# Enable camera
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 420)
+
+# import cascade file for facial recognition
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+'''
+    # if you want to detect any object for example eyes, use one more layer of classifier as below:
+    eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye_tree_eyeglasses.xml")
+'''
+
+while True:
+    success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Getting corners around the face
+    faces = faceCascade.detectMultiScale(imgGray, 1.3, 5)  # 1.3 = scale factor, 5 = minimum neighbor
+    # drawing bounding box around face
+    for (x, y, w, h) in faces:
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    '''
+    # detecting eyes
+    eyes = eyeCascade.detectMultiScale(imgGray)
+    # drawing bounding box for eyes
+    for (ex, ey, ew, eh) in eyes:
+        img = cv2.rectangle(img, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 3)
+    '''
+
+    cv2.imshow('face_detect', img)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyWindow('face_detect')
