6aabe84579f084818987228c42f3ead1b623601d
Pabitra Kumar <pabitrabera34@gmail.com>
2024-05-17 11:23:47 +0530

    Anti-Spoofing-WebApp ready

--- Anti_Spoofing_Project_using_DL/Model/convert.py
+++
@@ -1,7 +0,0 @@
-from keras.models import load_model
-
-# Load the model
-# model = load_model('anti_spoofing_project_using_dl.ipynb')
-
-# Save the model in HDF5 format
-model.save('anti_spoofing_project_using_dl.h5')
