16cf1d5f600e60874d0296af655d791792cdd237
Chirag <118044484+chiragHimself@users.noreply.github.com>
2024-05-25 18:02:10 +0530

    Reapply "Final commit"
    This reverts commit 34c1df987be099a21ef9a4e5eeae4f6da00bdfef.

--- Football Analyser YOLO/main.py
+++ Football Analyser YOLO/main.py
@@ -14,7 +14,7 @@ def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
     # Initialize Tracker
-    tracker = Tracker('models/newBest100.pt')
+    tracker = Tracker('models/newBest100.pt')#add your model name here as models/yourmodel.
 
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
