ecf435e3657c59976357d1d696bebfc833682201
Kanak <kanakverma325@gmail.com>
2025-02-24 13:05:58 +0000

    added movie-recommender system

---
+++ ChatGPT Reddit Comments Analysis using NLP/Movie_recommender_system/app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names,recommended_movie_posters
+
+movies_list = pickle.load(open('movies.pkl', 'rb'))
+movies = pd.DataFrame(movies_list)
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+st.title('Movie-recommender system')
+
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movies['title'].values
+)
+
+
+
+if st.button('Show Recommendation'):
+    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
---
+++ ChatGPT Reddit Comments Analysis using NLP/Movie_recommender_system/setup.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+port = $PORT\n\ 
+enableCORS = false\n\
+headless = true\n\
+\n\
+
+" > ~/.streamlit/config.toml
